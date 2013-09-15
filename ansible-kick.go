b2d6594235b23387bd9a452517c032eda8c576e1
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-09-15 08:52:23 -0700

    Add comments to the ansible-kick.go source file

--- ansible-kick.go
+++ ansible-kick.go
@@ -1,3 +1,6 @@
+// Copyright 2013 Kelsey Hightower. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
 package main
 
 import (
@@ -7,6 +10,8 @@ import (
 	"strings"
 )
 
+// getHost returns a string representing the IP address extracted from the
+// SSH_CLIENT environment variable.
 func getHost() string {
 	host := os.Getenv("SSH_CLIENT")
 	host = strings.Split(host, " ")[0]
@@ -18,8 +23,19 @@ func getHost() string {
 
 func main() {
 	flag.Parse()
+
+	// Don't trust the client to send it's own IP address; instead extract
+	// the IP from the SSH_CLIENT environment variable, which contains three
+	// space-separated pieces of information.  The IP address and source
+	// port of the client followed by the server's destination port number.
+	//
+	//  SSH_CLIENT = 203.0.113.10 4532 22
 	host := getHost()
+
+	// Disable printing of the timestamp and hostname when logging to the
+	// console.
 	log.SetFlags(0)
+
 	log.Print("starting ansible-kick for " + host)
 	if err := Run(host); err != nil {
 		log.Fatal(err.Error())
