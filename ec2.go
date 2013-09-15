37f354de6dd75b942ab77fb70a2505e4e92c7383
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-09-15 16:13:34 -0700

    Find EC2 instances by public or private IP address
    Before this commit ec2 instances where only searched by private IP
    address. Now instances are searched using the following logic:
    
    Using the IP address in SSH_CLIENT environment variable
    
     * Search for an instance filtering on private IP address
     * If no private IP match, then search filtering on public IP address

---
+++ ec2.go
@@ -0,0 +1,50 @@
+// Copyright 2013 Kelsey Hightower. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+package main
+
+import (
+	"errors"
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/ec2"
+)
+
+func searchByFilter(e *ec2.EC2, key, value string) (*ec2.Instance, error) {
+	filter := ec2.NewFilter()
+	filter.Add(key, value)
+	resp, err := e.Instances(nil, filter)
+	if err != nil {
+		return nil, errors.New("EC2 API call failed. " +
+			"Check your AWS credentials and system clock")
+	}
+	if len(resp.Reservations) != 1 {
+		return nil, errors.New("no instance with " + key + "=" + value)
+	} else {
+		return &resp.Reservations[0].Instances[0], nil
+	}
+}
+
+func (ac *ansibleConfig) findInstance() (*ec2.Instance, error) {
+	var (
+		err      error
+		instance *ec2.Instance
+	)
+
+	auth := aws.Auth{
+		ac.settings.AccessKeyId,
+		ac.settings.SecretAccessKey,
+	}
+	region := aws.Regions[ac.settings.Region]
+	e := ec2.New(auth, region)
+
+	instance, err = searchByFilter(e, "private-ip-address", ac.Host)
+	if err != nil {
+		instance, err = searchByFilter(e, "ip-address", ac.Host)
+	}
+
+	if err != nil {
+		return nil, err
+	} else {
+		return instance, nil
+	}
+}
