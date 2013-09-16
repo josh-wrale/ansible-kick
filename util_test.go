b82191deee60ffdb56bf5c87cf18dcfa7476bd9c
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-09-15 21:15:43 -0700

    Clean up util_test.go source file

--- util_test.go
+++ util_test.go
@@ -2,17 +2,17 @@ package main
 
 import "testing"
 
-var sshClientString = "203.0.113.100 4321 22"
-var invalidSSHClientString = "203.0.113.100"
+var (
+	sshClientString = "203.0.113.100 4321 22"
+	invalidSSHClientString = "203.0.113.100"
+)
 
 func TestExtractIP(t *testing.T) {
 	expect := "203.0.113.100"
-
 	got, _ := extractIP(sshClientString)
 	if got != expect {
 		t.Errorf("expected %s, got %s", expect, got)
 	}
-
 	got, err := extractIP(invalidSSHClientString)
 	if err.Error() != "invalid SSH_CLIENT format" {
 		t.Error("Expected err invalid SSH_CLIENT format, got ", err.Error())
